93d889ff1cff8d07154cd5702c98cfc69ae1f40d
Rafael Pontes <79320030+Rafael705@users.noreply.github.com>
2022-10-06 11:55:46 -0300

    Update Labamba_script.ino

--- Labamba_script.ino
+++ Labamba_script.ino
@@ -1,115 +1,58 @@
-#include <AFMotor.h>
-AF_DCMotor roda(1);
+#define pinBotaoA 5 // pino do button
 
-#define   dir_clk      4     //clock do registrador no digital  4
-#define   dir_ser      8     //dados do registrador no digital  8
-#define   dir_latch   12     //latch do registrador no digital 12
-#define   dir_en       7     //enable do registrador no digital 7
-
-#define   servo1      10     //controle do Servo 1
-
-void sendCommand(unsigned char value);   //Função para enviar o byte para saída do registrador
-void servo0graus();
-void servo90graus();
-void servo180graus();
+#define pinSentido1MotorA 3 // pino do motor
+#define pinSentido2MotorA 2 // pino motor 
 
 
-void setup()
-{
-  pinMode(dir_clk,   OUTPUT);    //saída para clock
-  pinMode(dir_ser,   OUTPUT);    //saída para dados
-  pinMode(dir_latch, OUTPUT);    //saída para latch
-  pinMode(dir_en,    OUTPUT);    //saída para enable
-  pinMode(servo1,    OUTPUT);    //saída para Servo 1
 
-  digitalWrite(dir_en, LOW);
-}
+byte estadoA = 0;
+bool estadoBotaoA = true;
+bool estadoAntBotaoA = true;
+unsigned long delayBotaoA;
+// foi criado Três variaves para o botão pq ele tem três estadoa.
+// Liga e gira no sendido horario, clica de novo e gira no sentido antihorario e clica de novo e desliga o brinquedo.
+
+void setup() {
+  
+  pinMode(pinBotaoA, INPUT_PULLUP);
+  pinMode(pinSentido1MotorA, OUTPUT);
+  pinMode(pinSentido2MotorA, OUTPUT);
  
-void loop()
-{
-
-  for(char i=0;i<100;i++) servo0graus();    //move o servo para a posição 0º por 2 segundos
-  for(char i=0;i<100;i++) servo90graus();    //move o servo para a posição 90º por 2 segundos
-  for(char i=0;i<100;i++) servo180graus();    //move o servo para a posição 180º por 2 segundos
-  for(char i=0;i<100;i++) servo90graus();    //move o servo para a posição 90º por 2 segundos
-
-  roda.setSpeed(255); //Define a velocidade maxima
-  roda.run(FORWARD); //Gira o motor sentido horario
- 
-  delay(3000);
-  roda.setSpeed(0);
-  roda.run(RELEASE); //Desliga o motor
- 
-  delay(3000);
-  roda.setSpeed(255);
-  roda.run(BACKWARD); //Gira o motor sentido anti-horario
- 
-  delay(3000);
-  roda.setSpeed(0);
-  roda.run(RELEASE); //Desliga o motor
- 
-  delay(4000); //Aguarda 5 segundos e repete o processo
 }
 
-void sendCommand(unsigned char value)
-{
-    boolean verify;                  //bit de controle
+void loop() {
+
+  estadoBotaoA = digitalRead(pinBotaoA);
+  if (!estadoBotaoA && estadoAntBotaoA) {
+     if ((millis() - delayBotaoA) > 20) {
+        estadoA++;
+        if (estadoA > 2) {
+           estadoA = 0;
+        }
+        delayBotaoA = millis();
+     }
+  } else if (estadoBotaoA != estadoAntBotaoA) {
+        delayBotaoA = millis();    
+  }
+  estadoAntBotaoA = estadoBotaoA;
+
  
-    digitalWrite(dir_latch, LOW);    //desliga latch
-    digitalWrite(dir_clk,   LOW);    //desabilita o clock
-     
-    for(unsigned char i=0x0; i<0x08; i++)    //loop finito para enviar os 8 bits
-    {
-       digitalWrite(dir_clk, LOW);   //clock em low
-        
-       if(value & (1<<i)) verify = HIGH;
-       else verify = LOW;
-        
-       digitalWrite(dir_ser, verify); //Habilita saída de dados conforme condição acima
-       digitalWrite(dir_clk, HIGH);
-     
-    } 
-    digitalWrite(dir_clk, LOW);
-    digitalWrite(dir_latch, HIGH);
+
+  if (estadoA == 0) {
     
+     digitalWrite(pinSentido1MotorA, LOW);
+     digitalWrite(pinSentido2MotorA, LOW);    
+
+  } else {   
+    
+     if (estadoA == 1) {
+        digitalWrite(pinSentido1MotorA, LOW);
+        digitalWrite(pinSentido2MotorA, HIGH);
+        
+     } else {//estadoA == 2
+       
+        digitalWrite(pinSentido1MotorA, HIGH);
+        digitalWrite(pinSentido2MotorA, LOW);      
+     }
+  } 
 }
- 
-void servo0graus()              //Posiciona o servo em 0 graus
-{
-   digitalWrite(servo1, HIGH);  //pulso do servo
-   delayMicroseconds(600);      //0.6ms
-   digitalWrite(servo1, LOW);   //completa periodo do servo
-   for(int i=0;i<32;i++)delayMicroseconds(600);
-   
-                               // 20ms = 20000us
-                               // 20000us - 600us = 19400us
-                               // 19400/600 = ~~32
- 
-}
- 
- 
-void servo90graus()             //Posiciona o servo em 90 graus
-{
-   digitalWrite(servo1, HIGH);  //pulso do servo
-   delayMicroseconds(1500);     //1.5ms
-   digitalWrite(servo1, LOW);   //completa periodo do servo
-   for(int i=0;i<12;i++)delayMicroseconds(1500);
-   
-                               // 20ms = 20000us
-                               // 20000us - 1500us = 18500us
-                               // 18500/1500 = ~~12 
- 
-}
- 
- 
-void servo180graus()             //Posiciona o servo em 180 graus
-{
-   digitalWrite(servo1, HIGH);  //pulso do servo
-   delayMicroseconds(2400);     //2.4ms
-   digitalWrite(servo1, LOW);   //completa periodo do servo
-   for(int i=0;i<7;i++)delayMicroseconds(2400);
-   
-                               // 20ms = 20000us
-                               // 20000us - 2400us = 17600us
-                               // 17600/2400 = ~~7
-} 
